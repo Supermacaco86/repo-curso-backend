edae2bec91739c818594c91c366ff320de3706ae
Supermacaco86 <99079977+Supermacaco86@users.noreply.github.com>
2023-01-17 13:14:28 -0300

    rutas con params

--- index.js
+++ index.js
@@ -11,11 +11,93 @@ app.get('/nueva-ruta',(req, res)=>{
 });
 
 app.get('/products',(req, res)=>{
-  res.json({
+  res.json([
+    {
     name: 'Product 1',
     price: 1000,
-  });
+  },
+  {
+    name: 'Product 2',
+    price: 2000,
+  }
+]);
 });
+
+app.get('/products/:id', (req, res)=>{
+  const {id} = req.params;
+  res.json({
+    id,
+    name: 'Preduct 2',
+    price: 2000,
+  })
+})
+
+// Endpoint con dos parametros dinamicos en la url:
+app.get('/categories/:categoryId/products/:productId', (req, res)=>{
+  const {categoryId, productId} = req.params;
+  res.json({
+    categoryId,
+    productId,
+  })
+})
+
+app.get('/purchaseOrders',(req, res)=>{
+  res.json([
+    {
+      name: 'purchase order1'
+    },
+    {
+      name: 'purchase order2'
+    },
+  ])
+})
+
+app.get('/purchaseOrders/:id',(req, res)=>{
+  const {id} = req.params;
+  res.json({
+    id,
+    name: 'purchase order'
+  })
+})
+
+app.get('/users',(req, res)=>{
+  res.json([
+    {
+    name: 'user1'
+  },
+  {
+    name: 'user2'
+  },
+])
+})
+
+app.get('/users/:id',(req, res)=>{
+  const {id} = req.params;
+  res.json({
+    id,
+    name: 'user'
+  })
+})
+
+app.get('/people',(req, res)=>{
+  res.json([
+    {
+    name: 'person1'
+  },
+  {
+    name: 'person2'
+  },
+])
+})
+
+app.get('/people/:id',(req, res)=>{
+  const {id} = req.params;
+  res.json({
+    id,
+    name: 'person'
+  })
+})
+
 
 app.listen(port, ()=>{
   console.log('Mi port ' + port);
