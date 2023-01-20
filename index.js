c8a5fa3356ec90106878c313cb2801e0c4a5ecd3
Supermacaco86 <99079977+Supermacaco86@users.noreply.github.com>
2023-01-19 23:08:41 -0300

    cors

--- index.js
+++ index.js
@@ -1,10 +1,23 @@
 const express = require('express');
-const routerApi = require('./routes')
+const cors = require('cors');
+const routerApi = require('./routes');
 const  {logErrors, errorHandler, boomErrorHandler} = require('./middleware/errorHandler')
 const app = express();
 const port = 3000;
 
 app.use(express.json());
+
+const whiteList = ['http://localhost:8080'];
+const optios = {
+  origin: (origin, callback) => {
+    if(whiteList.includes(origin)){
+      callback(null, true)
+    }else{
+      callback(new Error('No permitido.'))
+    }
+  }
+}
+app.use(cors(optios));
 
 app.get('/',(req, res)=>{
   res.send('Hola mi server en express.');
